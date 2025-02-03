1810cbe8bbbd54b3b2ae86a0dcaf9a9bafaa59e6
Cassiano Santos <cassianosantosneto@gmail.com>
2025-02-03 19:42:43 -0300

    refact: fixin feature frame in small pages

--- src/app/components/window-control-circles/window-control-circles.tsx
+++ src/app/components/window-control-circles/window-control-circles.tsx
@@ -1,21 +1,48 @@
 import { HeaderProps } from "@/app/page";
+import { useEffect, useState } from "react";
 import "./window-control-circles.css";
 
 export default function WindowControlCircles({ onChangeViewMode }: HeaderProps) {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 1250 && window.innerHeight < 720);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return (
     <div className="window-control-circles">
-      <span
-        className="circle red"
-        onClick={() => onChangeViewMode("normal")}
-      />
-      <span
-        className="circle yellow"
-        onClick={() => onChangeViewMode("iphone")}
-      />
-      <span
-        className="circle green"
-        onClick={() => onChangeViewMode("macbook")}
-      />
+      <div className="tooltip-container">
+        <span
+          className={`circle red ${isSmallScreen ? "disabled" : ""}`}
+          onClick={() => !isSmallScreen && onChangeViewMode("normal")}
+        />
+        {isSmallScreen && <span className="tooltip">Funcionalidade para telas maiores</span>}
+      </div>
+
+      <div className="tooltip-container">
+        <span
+          className={`circle yellow ${isSmallScreen ? "disabled" : ""}`}
+          onClick={() => !isSmallScreen && onChangeViewMode("iphone")}
+        />
+        {isSmallScreen && <span className="tooltip">Funcionalidade para telas maiores</span>}
+      </div>
+
+      <div className="tooltip-container">
+        <span
+          className={`circle green ${isSmallScreen ? "disabled" : ""}`}
+          onClick={() => !isSmallScreen && onChangeViewMode("macbook")}
+        />
+        {isSmallScreen && <span className="tooltip">Funcionalidade para telas maiores</span>}
+      </div>
     </div>
   );
 }
